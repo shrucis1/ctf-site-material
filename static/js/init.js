e134a7afe8647c6f5d8c057309869a6b595a740d
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-03-04 18:38:22 -0500

    add scrolling animation

--- static/js/init.js
+++ static/js/init.js
@@ -3,6 +3,17 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('a[href*=#]').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var $target = $(this.hash);
+			$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+			 if ($target.length) {
+			 	var targetOffset = $target.offset().top - 100;
+				$('html,body').animate({scrollTop: targetOffset}, 1000);
+				return false;
+			}
+		}
+	});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space

44368205dcc629eb82ae06850fd4dd654f7cc833
James Woglom <j@wogloms.net>
2016-03-04 18:54:27 -0500

    fix scrolling javascript

--- static/js/init.js
+++ static/js/init.js
@@ -1,19 +1,17 @@
-(function($){
-  $(function(){
+$(document).ready(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('a[href*=#]').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var $target = $(this.hash);
-			$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-			 if ($target.length) {
-			 	var targetOffset = $target.offset().top - 100;
-				$('html,body').animate({scrollTop: targetOffset}, 1000);
-				return false;
-			}
-		}
-	});
+    $("a[href*='#']").click(function() {
+    	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    		var $target = $(this.hash);
+    		$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+    		 if ($target.length) {
+    		 	var targetOffset = $target.offset().top - 100;
+    			$('html,body').animate({scrollTop: targetOffset}, 500);
+    			return false;
+    		}
+    	}
+    });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+}); // end of document ready

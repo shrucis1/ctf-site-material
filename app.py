98b7ee3c304634432a6e6649a51e328dfe50e90a
Fox Wilson <fwilson@fwilson.me>
2016-03-04 19:40:31 -0500

    app.py: remove another step in the if-else ladder

--- app.py
+++ app.py
@@ -5,22 +5,21 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        email_address = request.form['email']
-        if not validate_email(email_address):
-            return render_template("index.html", invalid_email=True)
+    if not request.method == 'POST':
+        return render_template("index.html")
 
-        if not add_user_to_list(name, email_address, "ctfmembers"):
-            return render_template("index.html", add_error=True)
+    name = request.form['name']
+    email_address = request.form['email']
+    if not validate_email(email_address):
+        return render_template("index.html", invalid_email=True)
 
-        if not send_confirmation_email(name, email_address):
-            return render_template("index.html", conf_email_error=True)
+    if not add_user_to_list(name, email_address, "ctfmembers"):
+        return render_template("index.html", add_error=True)
 
-        return render_template("index.html", success=True)
+    if not send_confirmation_email(name, email_address):
+        return render_template("index.html", conf_email_error=True)
 
-    else:
-        return render_template('index.html')
+    return render_template("index.html", success=True)
 
 if __name__ == '__main__':
     app.run(port=9020)

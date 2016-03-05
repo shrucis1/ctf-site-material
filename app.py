b04d3dcb214bc2917f6078f7584756d5ee7945bf
Fox Wilson <fwilson@fwilson.me>
2016-03-04 19:37:27 -0500

    app.py: refactor if-else tree, move list creation logic to a separate script

--- app.py
+++ app.py
@@ -6,32 +6,19 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if validate_email(request.form['email']):
-            name = request.form['name']
-            email_address = request.form['email']
-            if not get_list('ctfmembers'):
-                if not create_list('ctfmembers'):
-                    print("Error Creating Mailing List")
-                    return render_template('index.html', create_list_error=True)
-                else:
-                    print("Mailing List Created")
-                if add_user_to_list(name, email_address, 'ctfmembers'):
-                    if send_confirmation_email(name, email_address):
-                        return render_template('index.html', success=True)
-                    else:
-                        return render_template('index.html', conf_email_error=True)
-                else:
-                    return render_template('index.html', add_error=True)
-            else:
-                if add_user_to_list(name, email_address, 'ctfmembers'):
-                    if send_confirmation_email(name, email_address):
-                        return render_template('index.html', success=True)
-                    else:
-                        return render_template('index.html', conf_email_error=True)
-                else:
-                    return render_template('index.html', add_error=True)
-        else:
-            return render_template('index.html', invalid_email=True)
+        name = request.form['name']
+        email_address = request.form['email']
+        if not validate_email(email_address):
+            return render_template("index.html", invalid_email=True)
+
+        if not add_user_to_list(name, email_address, "ctfmembers"):
+            return render_template("index.html", add_error=True)
+
+        if not send_confirmation_email(name, email_address):
+            return render_template("index.html", conf_email_error=True)
+
+        return render_template("index.html", success=True)
+
     else:
         return render_template('index.html')
 

d9368043df2a2466637b7cce6a09dec87e71a597
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-03-13 11:17:37 -0400

    change image for about

---
+++ mailgun_config.py
@@ -0,0 +1,5 @@
+mailgun_url = 'https://api.mailgun.net/v3/<site_url>'
+mailgun_key = 'key-abcdefghijklmnopqrstuvwxyz1234567890'
+pub_key = 'pubkey-1234567890qwertyuiop'
+site_url = ''
+list_name = 'ctfmembers'
